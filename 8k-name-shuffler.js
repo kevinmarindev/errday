4cb0178d82a5bda3d26b80447538c28fe22a0be5
Kevin Marin <kevin.marin7@hotmail.com>
2021-02-24 00:28:28 -0600

    used array and string methods

---
+++ 8k-name-shuffler.js
@@ -0,0 +1,15 @@
+
+//
+// function nameShuffler(str){
+// let stepone = str.split(' ')
+// let steptwo =  stepone.reverse()
+// let stepthree = steptwo.join(' ')
+//
+// }
+// nameShuffler('Kevin Marin')
+
+
+
+function nameShuffler(str){
+return str.split(' ').reverse().join(' ')
+}
