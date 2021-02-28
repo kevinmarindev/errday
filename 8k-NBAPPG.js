d1ea0f10c30ec370b7ce97466e6e80c8dd90190c
Kevin Marin <kevin.marin7@hotmail.com>
2021-02-28 00:23:49 -0600

    changes were made

--- 8k-NBAPPG.js
+++ 8k-NBAPPG.js
@@ -19,21 +19,3 @@
 // let spt = spl.split(" ")
 // console.log(spt.length)
 // console.log(spt[0].length)
-
-function findLongest(str) {
-
-  var spl = str.split(" ");
-  let longest = 0
-
-  for (let i = 0; i < spl.length; i++){
-    if (spl[i].length > longest) {
-      longest = spl[i].length
-
-    }}
-    console.log(longest)
-    console.log(spl[1].length)
-    console.log(spl.length)
-    console.log(spl)
-
-}
-findLongest('this is the house of habukah')
