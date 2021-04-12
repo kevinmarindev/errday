f5e534cb6d41e5786bb0e7386c6fefd1446f46d0
kevinmarindev <kevin.marin7@hotmail.com>
2021-04-12 18:02:58 -0500

    given a num return the muliplication table.

---
+++ 8k-multiplactionTable.js
@@ -0,0 +1,12 @@
+function multiTable(number) {
+  return `1 * ${number} = ${number * 1}\n2 * ${number} = ${
+    number * 2
+  }\n3 * ${number} = ${number * 3}\n4 * ${number} = ${
+    number * 4
+  }\n5 * ${number} = ${number * 5}\n6 * ${number} = ${
+    number * 6
+  }\n7 * ${number} = ${number * 7}\n8 * ${number} = ${
+    number * 8
+  }\n9 * ${number} = ${number * 9}\n10 * ${number} = ${number * 10}`;
+}
+console.log(multiTable(10))
