b23db337c62fcea546670c3fa2fb199582bffd13
kevinmarindev <kevin.marin7@hotmail.com>
2021-06-13 01:45:21 -0500

    understanding other ways to solve this kata.

--- 7k-separateDataTypes.js
+++ 7k-separateDataTypes.js
@@ -1,29 +1,32 @@
-function separateTypes(input) {
-let theReturn = {};
-
-input.forEach((item)=>{
-    console.log(typeof item)
-    if(!theReturn[typeof item]){
-        theReturn[typeof item] = []
-    }
-    if(theReturn[typeof item]){
-         typeof item === 'number' ? theReturn.number.push(item) : typeof item === 'string' ? theReturn.string.push(item) : typeof item === 'boolean' ? theReturn.boolean.push(item) :
-    null;
-    }
-    else null
-})
-
-return theReturn
-
-
 // function separateTypes(input) {
-//   return input.reduce((types, value) => {
-//     const valueType = typeof value
-//     const type = types[valueType]
-//     types[valueType] = type ? [...type, value] : [value]
-//     return types
-//   }, {})
-// }
+// let theReturn = {};
+
+// input.forEach((item)=>{
+//     console.log(typeof item)
+//     if(!theReturn[typeof item]){
+//         theReturn[typeof item] = []
+//     }
+//     if(theReturn[typeof item]){
+//          typeof item === 'number' ? theReturn.number.push(item) : typeof item === 'string' ? theReturn.string.push(item) : typeof item === 'boolean' ? theReturn.boolean.push(item) :
+//     null;
+//     }
+//     else null
+// })
+
+// return theReturn
+
+
+function separateTypes(input) {
+  return input.reduce((types, value) => {
+    const valueType = typeof value
+    console.log(valueType)
+    // console.log(types)
+    const type = types[valueType]
+    console.log(types[valueType])
+    types[valueType] = type ? [...type, value] : [value]
+    return types
+  }, {})
+}
 
 // let arr = ['k', 'e', 'v']
 
@@ -34,6 +37,6 @@ return theReturn
 // console.log(tt)
 
 
-}
+// }
 
 console.log(separateTypes(['a', 1, 2, 'b', false]))
