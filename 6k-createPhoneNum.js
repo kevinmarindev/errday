7d019bf1c854f72f4f61af596b712d2950c15063
kevinmarindev <kevin.marin7@hotmail.com>
2021-12-05 22:41:51 -0600

    given 10 digits in an arr return a string formated as a phone num

---
+++ 6k-createPhoneNum.js
@@ -0,0 +1,4 @@
+function create_phone_number(arr){
+   return `(${arr[0]}${arr[1]}${arr[2]}) ${arr[3]}${arr[4]}${arr[5]}-${arr[6]}${arr[7]}${arr[8]}${arr[9]}` 
+}
+console.log(nameIt([1,2,3,4,5,6,7,8,9,0]))
